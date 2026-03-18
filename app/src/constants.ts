9374d073f14885b630a4c5cd515ad5fe14a7ddd4
IMED458 <imedashvili700@gmail.com>
2026-03-18 23:29:30 +0400

    Remove standalone ABCDFWC resource entry

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -132,13 +132,5 @@ export const RESOURCES: ResourceLink[] = [
     url: 'https://imed458.github.io/nurse.github.io/',
     icon: 'BriefcaseMedical',
     access: 'ვებ',
-  },
-  {
-    id: '14',
-    title: 'პაციენტის პირველადი საექთნო ABCDFWC შეფასება გადაუდებელი დახმარების დეპარტამენტში',
-    description: 'საექთნო შეფასების დასაბეჭდი ფორმა',
-    url: './nursing/primary-abcdfwc-assessment.html',
-    icon: 'FileText',
-    access: 'ვებ',
   }
 ];
